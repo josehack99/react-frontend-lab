bcdfe40389efd12a7ce2c1791643458138526ec6
josehack99 <ferchosertess@gmail.com>
2026-03-01 10:17:23 -0600

    testeando el correo

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -6,6 +6,7 @@ import { PreviousSearches } from './gifs/components/PreviousSearches'
 import { GifList } from './gifs/components/GifList'
 import { getGifsByQuery } from './gifs/actions/get-gifs-by-query.action'
 import type { Gif } from './gifs/interfaces/gif.interface'
+//g
 //
 export const GifsApp = () => {
 //   //ferchoss99
